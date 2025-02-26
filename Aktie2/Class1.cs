a163465566cf53d9c345b7b0af8b550699ebb339
Jonas Büchner <JonasBuchner36@gmail.com>
2025-02-26 12:46:33 +0100

    Add project files.

---
+++ Aktie2/Class1.cs
@@ -0,0 +1,40 @@
+﻿namespace Aktie
+{
+    /// <summary>
+    /// Repræsenterer en aktie med dens navn, antal aktier, købs- og salgskurs.
+    /// </summary>
+    public class Stock
+    {
+        /// <summary>
+        /// Får eller sætter navnet på aktien.
+        /// </summary>
+        public string AktieNavn { get; set; }
+
+        /// <summary>
+        /// Får eller sætter antallet af aktier.
+        /// </summary>
+        public int antalAktier { get; set; }
+
+        /// <summary>
+        /// Får eller sætter købsprisen pr. aktie.
+        /// </summary>
+        public int KøbsKurs { get; set; }
+
+        /// <summary>
+        /// Får eller sætter salgsprisen pr. aktie.
+        /// </summary>
+        public int SalgsKurs { get; set; }
+
+
+        /// <summary>
+        /// Beregner overskud eller tab på den pågældende aktie.
+        /// </summary>
+        /// <returns>Overskud eller tab beregnet som (SalgsKurs - KøbsKurs) * antalAktier.</returns>
+        public int Afkast()
+        {
+            return (SalgsKurs - KøbsKurs) * antalAktier;
+        }
+
+
+    }
+}
---
+++ TestAfAfkastmetode/StockTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aktie;
+using System;
+
+namespace Aktie.Tests
+{
+    [TestClass()]
+    public class StockTests
+    {
+        [TestMethod()]
+        public void AfkastTest()
+        {
+            // Arrange
+            var stock = new Stock
+            {
+                AktieNavn = "Test Aktie",
+                KøbsKurs = 100,
+                SalgsKurs = 150,
+                antalAktier = 10
+            };
+
+            // Act
+            var result = stock.Afkast();
+
+            // Assert
+            Assert.AreEqual(500, result);
+        }
+
+        [TestMethod()]
+        public void AktieNavnTest()
+        {
+            // Arrange
+            var stock = new Stock();
+            var expectedName = "Test Aktie";
+
+            // Act
+            stock.AktieNavn = expectedName;
+
+            // Assert
+            Assert.AreEqual(expectedName, stock.AktieNavn);
+        }
+
+        [TestMethod()]
+        public void AntalAktierTest()
+        {
+            // Arrange
+            var stock = new Stock();
+            var expectedAntal = 100;
+
+            // Act
+            stock.antalAktier = expectedAntal;
+
+            // Assert
+            Assert.AreEqual(expectedAntal, stock.antalAktier);
+        }
+
+        [TestMethod()]
+        public void KøbsKursTest()
+        {
+            // Arrange
+            var stock = new Stock();
+            var expectedKøbsKurs = 200;
+
+            // Act
+            stock.KøbsKurs = expectedKøbsKurs;
+
+            // Assert
+            Assert.AreEqual(expectedKøbsKurs, stock.KøbsKurs);
+        }
+
+        [TestMethod()]
+        public void SalgsKursTest()
+        {
+            // Arrange
+            var stock = new Stock();
+            var expectedSalgsKurs = 250;
+
+            // Act
+            stock.SalgsKurs = expectedSalgsKurs;
+
+            // Assert
+            Assert.AreEqual(expectedSalgsKurs, stock.SalgsKurs);
+        }
+    }
+}
